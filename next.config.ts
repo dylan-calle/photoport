44348c0cc210e07da09caf040e7098c4daeef565
Dylan <dylancc20@gmail.com>
2025-07-06 00:43:30 -0400

    refactor: change sites to remotePatterns bc it was deprecated

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
+// next.config.ts or next.config.js
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["res.cloudinary.com"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/**", // allow all paths
+      },
+    ],
   },
 };
 
